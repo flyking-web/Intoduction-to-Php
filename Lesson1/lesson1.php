404451f006b5457940c9650a040993bf7bc6e07d
flyking-web <craigkamunyu.com>
2020-02-04 11:40:27 +0300

    First try

---
+++ Lesson1/lesson1.php
@@ -0,0 +1,31 @@
+<?php
+//introduction to Php
+//PHP is a widely-used, open source scripting language
+//PHP scripts are executable on the server
+//PHP files can contain text, HTML, CSS, Javascript, and PHP code
+//PHP code is executable on the server, and the result is returned to the browser as plain HTML
+//PHP files have extension ."php"
+echo "Hello world";
+echo"<h1>Hello world</h1>";
+//echo:print content in browser
+//Hello world:content to be printed on the browser
+//; :means end of php statement or instruction
+
+
+//VARIABLES IN PHP
+//a container for storing data
+$myname="Cassius Ohno <br>";
+echo $myname;
+
+$company1="Sumsung <br>";
+echo $company1;
+$company2="Toyota <br>";
+echo $company2;
+$company3="Google <br>";
+echo $company3;
+
+?>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab
+    autem blanditiis dolore ducimus ea, eum illo laboriosam laudantium magnam maiores maxime
+    mollitia qui quibusdam quo quod ratione, saepe sequi veniam.
+</p>
